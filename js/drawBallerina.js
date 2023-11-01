b9b40ea946f29dda013b776d99dda32b63dff234
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2023-11-01 13:01:24 +0100

    ballerina dance animation

--- js/drawBallerina.js
+++ js/drawBallerina.js
@@ -6,6 +6,7 @@ function drawBallerina() {
   rotateX(90)
   translate(0, BOTTOM_BOX_HEIGHT / 2 + WALL_THICKNESS / 2 + BALLERINA_APPROXIMATE_HEIGHT / 2 + platformLevelOffset, 0)
   scale(BALLERINA_SCALE)
+  rotateY(millis() * 0.1)
   normalMaterial()
   model(ballerina)
   pop()
