0b34ddee84f5becf1789b02ce764ef6c0a075b7a
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2023-11-02 19:57:14 +0100

    ballerina moved a little to the back

--- js/drawBallerina.js
+++ js/drawBallerina.js
@@ -4,7 +4,7 @@ const BALLERINA_APPROXIMATE_HEIGHT = BALLERINA_SCALE * 200
 function drawBallerina() {
   push()
   rotateX(90)
-  translate(0, BOTTOM_BOX_HEIGHT / 2 + WALL_THICKNESS / 2 + BALLERINA_APPROXIMATE_HEIGHT / 2 + platformLevelOffset, 0)
+  translate(0, BOTTOM_BOX_HEIGHT / 2 + WALL_THICKNESS / 2 + BALLERINA_APPROXIMATE_HEIGHT / 2 + platformLevelOffset, BOX_DEPTH / 2 * 0.2)
   scale(BALLERINA_SCALE)
   rotateY(millis() * 0.1)
   normalMaterial()
