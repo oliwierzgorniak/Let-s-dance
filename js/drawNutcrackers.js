dfb679898417a0b9e9adaac75e12a1a7b58f8dde
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2023-11-02 13:37:36 +0100

    towers hiding animation

--- js/drawNutcrackers.js
+++ js/drawNutcrackers.js
@@ -3,44 +3,54 @@ const NUTCRACKER_APPROXIMATE_HEIGHT = BALLERINA_SCALE * 200
 const TOWER_CYLINDER_RADIUS = 10;
 const TOWER_CYLINDER_HEIGHT = 32;
 
-let nutcrackerOffset = 0;
+let nutcrackersOffset = 0;
+let nutcrackerTowersOffset = 0;
+let towersRevealed = false;
 
-function drawNutcrackers() {  
-  if (nutcrackerOffset< TOWER_CYLINDER_HEIGHT && platformLevelOffset >= MAX_PLATFORM_LEVEL_OFFSET) {
-    nutcrackerOffset += 1;
+function drawNutcrackers() {
+  if (!towersRevealed && nutcrackersOffset < TOWER_CYLINDER_HEIGHT && platformLevelOffset >= MAX_PLATFORM_LEVEL_OFFSET) {
+    nutcrackersOffset += 0.4;
+    nutcrackerTowersOffset += 0.4;
   }
 
+  if (nutcrackersOffset >= TOWER_CYLINDER_HEIGHT) towersRevealed = true;
+
+  
   push()
   rotateX(90)
-  translate(NUTCRACKER_POSITION, BOTTOM_BOX_HEIGHT / 2 + WALL_THICKNESS / 2 + NUTCRACKER_APPROXIMATE_HEIGHT / 2 + platformLevelOffset - TOWER_CYLINDER_HEIGHT, (BOX_DEPTH - WALL_THICKNESS * 2) / 2 * 0.7)
+  translate(NUTCRACKER_POSITION, BOTTOM_BOX_HEIGHT / 2 + WALL_THICKNESS / 2 + NUTCRACKER_APPROXIMATE_HEIGHT / 2 + platformLevelOffset - TOWER_CYLINDER_HEIGHT + nutcrackersOffset, (BOX_DEPTH - WALL_THICKNESS * 2) / 2 * 0.7)
   scale(NUTCRACKER_SCALE)
   // rotateY(millis() * 0.1)
   normalMaterial()
   model(nutcrackerModel)
   pop()
-
+  
   push()
   rotateX(90)
-  translate(-NUTCRACKER_POSITION, BOTTOM_BOX_HEIGHT / 2 + WALL_THICKNESS / 2 + NUTCRACKER_APPROXIMATE_HEIGHT / 2 + platformLevelOffset - TOWER_CYLINDER_HEIGHT, (BOX_DEPTH - WALL_THICKNESS * 2) / 2 * 0.7)
+  translate(-NUTCRACKER_POSITION, BOTTOM_BOX_HEIGHT / 2 + WALL_THICKNESS / 2 + NUTCRACKER_APPROXIMATE_HEIGHT / 2 + platformLevelOffset - TOWER_CYLINDER_HEIGHT + nutcrackersOffset, (BOX_DEPTH - WALL_THICKNESS * 2) / 2 * 0.7)
   scale(NUTCRACKER_SCALE)
   // rotateY(millis() * 0.1)
   normalMaterial()
   model(nutcrackerModel)
   pop()
-
+  
   drawNutcrackerTowers()
 }
 
 function drawNutcrackerTowers() {
+  if (towersRevealed && nutcrackersOffset >= 0) {
+    nutcrackerTowersOffset -= 0.2;
+  }
+
   push()
   rotateX(90)
-  translate(NUTCRACKER_POSITION, BOTTOM_BOX_HEIGHT / 2 + TOWER_CYLINDER_HEIGHT / 2 + platformLevelOffset - TOWER_CYLINDER_HEIGHT + nutcrackerOffset, (BOX_DEPTH - WALL_THICKNESS * 2) / 2 * 0.7)
+  translate(NUTCRACKER_POSITION, BOTTOM_BOX_HEIGHT / 2 + TOWER_CYLINDER_HEIGHT / 2 + platformLevelOffset - TOWER_CYLINDER_HEIGHT + nutcrackerTowersOffset, (BOX_DEPTH - WALL_THICKNESS * 2) / 2 * 0.7)
   cylinder(TOWER_CYLINDER_RADIUS, TOWER_CYLINDER_HEIGHT)
   pop()
   
   push()
   rotateX(90)
-  translate(-NUTCRACKER_POSITION, BOTTOM_BOX_HEIGHT / 2 + TOWER_CYLINDER_HEIGHT / 2 + platformLevelOffset - TOWER_CYLINDER_HEIGHT + nutcrackerOffset, (BOX_DEPTH - WALL_THICKNESS * 2) / 2 * 0.7)
+  translate(-NUTCRACKER_POSITION, BOTTOM_BOX_HEIGHT / 2 + TOWER_CYLINDER_HEIGHT / 2 + platformLevelOffset - TOWER_CYLINDER_HEIGHT + nutcrackerTowersOffset, (BOX_DEPTH - WALL_THICKNESS * 2) / 2 * 0.7)
   cylinder(TOWER_CYLINDER_RADIUS, TOWER_CYLINDER_HEIGHT)
   pop()
 }
