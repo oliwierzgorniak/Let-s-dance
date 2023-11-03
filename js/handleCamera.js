47d2b28d5f3932f0ccba01bfefe22df67767642f
Oliwier Zgórniak <oliwier.zgorniak@protonmail.com>
2023-11-03 01:53:51 +0100

    camera movment done

--- js/handleCamera.js
+++ js/handleCamera.js
@@ -4,6 +4,8 @@ let myCamera;
 
 
 function handleCamera() {
+  console.log(cameraMovmentPhase)
+
 if (animationStarted) {
   if (cameraMovmentPhase === 0 && cameraProps.z > TABLE_HEIGHT + BOTTOM_BOX_HEIGHT + 1 && cameraProps.y > 200) {
     cameraProps.y -= 0.34;
@@ -11,61 +13,56 @@ if (animationStarted) {
   } else if (cameraMovmentPhase === 0) cameraMovmentPhase = 1;
 }
   
-  if (cameraMovmentPhase === 1 && cameraProps.x > - TABLE_SURFACE_RADIUS * 0.7) {
-    cameraProps.x -= 0.57;
+  if (cameraMovmentPhase === 1 && cameraProps.x > - TABLE_SURFACE_RADIUS * 0.65) {
+    cameraProps.x -= 0.496;
     cameraProps.y -= 0.3;
   } else if (cameraMovmentPhase === 1) cameraMovmentPhase = 2;
   
   if (cameraMovmentPhase === 2 && cameraProps.x < 0) {
-    cameraProps.x += 0.57;
+    cameraProps.x += 0.496;
     cameraProps.y += 0.3;
   } else if (cameraMovmentPhase === 2) cameraMovmentPhase = 3;
 
-  if (cameraMovmentPhase === 3 && cameraProps.x < TABLE_SURFACE_RADIUS * 0.7) {
-    cameraProps.x += 0.57;
+  if (cameraMovmentPhase === 3 && cameraProps.x < TABLE_SURFACE_RADIUS * 0.65) {
+    cameraProps.x += 0.496;
     cameraProps.y -= 0.3;
   } else if (cameraMovmentPhase === 3) cameraMovmentPhase = 4;
   
   if (cameraMovmentPhase === 4 && cameraProps.x > 0) {
-    cameraProps.x -= 0.57;
+    cameraProps.x -= 0.496;
     cameraProps.y += 0.3;
   } else if (cameraMovmentPhase === 4) cameraMovmentPhase = 5;
 
-  if (cameraMovmentPhase === 5 && cameraProps.z < TABLE_HEIGHT + BOTTOM_BOX_HEIGHT + 100 ) {
+  if (cameraMovmentPhase === 5 && cameraProps.z < TABLE_HEIGHT + BOTTOM_BOX_HEIGHT + 150 ) {
     cameraProps.z += 1;
-    cameraProps.y -= 0.5;
-    cameraProps.zCentral += 0.1
+    cameraProps.y -= 0.8;
+    cameraProps.zCentral += 0.5
   } else if (cameraMovmentPhase === 5) cameraMovmentPhase = 6;
 
   
   if (cameraMovmentPhase === 6 && cameraProps.y > -BACK_WALL_POSITION * 0.2 ) {
-    cameraProps.yCentral -= 1;
-    cameraProps.y -= 1;
+    cameraProps.y -= 1.9;
+    cameraProps.z -= 0.5;
+    cameraProps.yCentral -= 2.4;
+    cameraProps.zCentral -= 0.3;
+
   } else if (cameraMovmentPhase === 6) cameraMovmentPhase = 7;
   
   if (cameraMovmentPhase === 7 && cameraProps.zCentral > TABLE_HEIGHT + WINDOW_HEIGHT / 2 - 50 ) {
-    cameraProps.y += 0.4;
-    cameraProps.z -= 1;
-    cameraProps.zCentral -= 1
+    cameraProps.y += 4;
+    cameraProps.z -= 2.6;
+    cameraProps.zCentral -= 2.8
   } else if (cameraMovmentPhase === 7) cameraMovmentPhase = 8;
+
 
 if (
   cameraMovmentPhase === 8 &&
-  cameraProps.z > TABLE_HEIGHT + BOTTOM_BOX_HEIGHT + 13
-) {
-  cameraProps.y -= 1;
-  cameraProps.z -= 1;
-  cameraProps.yCentral -= 0.15
-} else if (cameraMovmentPhase === 8) cameraMovmentPhase = 9;
-
-if (
-  cameraMovmentPhase === 9 &&
   cameraProps.y > -BACK_WALL_POSITION * 0.58
 ) {
-  cameraProps.y -= 1;
-  cameraProps.yCentral -= 1
-  cameraProps.zCentral -= 0.039
-} else if (cameraMovmentPhase === 9) cameraMovmentPhase = 10;
+  cameraProps.y -= 2.1;
+  cameraProps.yCentral -= 2.2
+  cameraProps.zCentral += 0.11
+} else if (cameraMovmentPhase === 8) cameraMovmentPhase = 9;
 
   camera(cameraProps.x, cameraProps.y, cameraProps.z, cameraProps.xCentral, cameraProps.yCentral, cameraProps.zCentral, cameraProps.x * 5.7, 1000, 0)
 
